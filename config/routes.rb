6d693734739ec0e1edfcf7ede179354b6805110a
Knmr-1120 <273.15.kt@gmail.com>
2020-10-22 19:17:51 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # [HTTPメソッド] '[URIパターン]', to: '[コントローラー名]#[アクション名]'
-  get 'posts', to: 'posts#index'
-  # リクエスト
-  # GETのHTTPメソッド（ただ単にトップページを表示するため）
-  # URLはhttp://localhost:3000/posts
-  # 行き先
-  # postsコントローラーという名前のコントローラー
-  # indexアクションという名前のアクション
-  get 'posts/new', to: 'posts#new'
+  root to: 'posts#index'
   post 'posts', to: 'posts#create'
 end
